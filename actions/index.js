2c1e8f18e81a25828a64ff6544cf9470f642a504
johnski9000 <januszwozniak.bth@gmail.com>
2022-03-21 12:45:05 +0000

    third commit

---
+++ actions/index.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+
+export const useGetPosts = () => {
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    async function getPosts() {
+      const res = await fetch('/api/v1/posts');
+      const result = await res.json();
+      if (res.status !== 200) {
+        setError(result)
+      } else {
+        setPosts(result);
+      }
+      setLoading(false)
+    }
+
+    getPosts();
+  }, [])
+
+  return {posts, error, loading}
+}
--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -1,60 +1,111 @@
-import BaseLayout from "../../components/layouts/BaseLayout";
-import axios from "axios"
-import Link from "next/link";
-import BasePage from "../../components/BasePage"
-import { useEffect, useState } from 'react';
+// import BaseLayout from "../../components/layouts/BaseLayout";
+// import axios from "axios"
+// import Link from "next/link";
+// import BasePage from "../../components/BasePage"
+// // import {useGetPosts} from "@/actions"
+// import { useEffect, useState } from 'react';
 
-function Portfolios() {
-  const [posts, setPosts] = useState([])
+// function Portfolios() {
+// // const {posts} = useGetPosts();
+// const [posts, setPosts] = useState([])
+// const [loading, setLoading] = useState(true)
+// // const [error, setError] = useState([])
+//     useEffect(() => {
+//     async function fetchPosts() {
+//       const res = await fetch("/api/v1/posts")
+//       const data = await res.json()
+//       setPosts(data)
+//     }
+//     setLoading(false)
 
-  useEffect(() => {
-    async function fetchPosts() {
-      const res = await fetch("/api/v1/posts")
-      const data = await res.json()
-      setPosts(data)
-    }
-    fetchPosts()
-  }, [])
+//     fetchPosts()
+//   }, [])
 
+//   const renderPosts = () => {
+//     return posts.map(post =>
+//       <li key={post.id} style={{'fontSize': '20px'}}>
+//         <Link as={`/portfolios/${post.id}`} href="/portfolios/[id]">
+//           <a>
+//             {post.title}
+//           </a>
+//         </Link>
+//       </li>
+//     )
+//   }
 
-  const renderPosts = () => {
-    return posts.map((post) => 
-    <li key={post.id}>
-      <Link href={"/portfolios/" + post.id}>
-        <a>
-          {post.title}
-        </a>
-      </Link>
-    </li>
-    
-        
+//   return (
+//     <BaseLayout>
+//     <BasePage>
+//     <h1>
+//       I am portfolio page
+//     </h1>
+//     { loading && 
+//     <p>loading...</p>
+//     }
+//     { posts && 
+//     <ul>
+//      {renderPosts()}
+//     </ul>
+//     }
+//     {/* {error && 
+//     <div className="alert alert-danger">{error.message}</div>
+//     } */}
+//     </BasePage>
+      
+//     </BaseLayout>
+//   )
+// }
+
+// // Portfolios.getInitialProps = async () => {
+// //   let posts = [];
+// // try {
+// //   const res = await axios.get("https://jsonplaceholder.typicode.com/posts")
+// //   posts = res.data;
+// // } catch(e) {
+// //   console.error(e)
+// // }
+// //   return {posts: posts.slice(0,10)}
+// // }
+
+// export default Portfolios
+
+import BaseLayout from '@/components/layouts/BaseLayout';
+import BasePage from '@/components/BasePage';
+import Link from 'next/link';
+import { useGetPosts } from '@/actions';
+
+const Portfolios = () => {
+  const { posts, error, loading } = useGetPosts();
+  const renderPosts = (posts) => {
+    return posts.map(post =>
+      <li key={post.id} style={{'fontSize': '20px'}}>
+        <Link as={`/portfolios/${post.id}`} href="/portfolios/[id]">
+          <a>
+            {post.title}
+          </a>
+        </Link>
+      </li>
     )
   }
 
   return (
     <BaseLayout>
-    <BasePage>
-    <h1>
-        I am portfolio page
-      </h1>
-      <ul>
-        {renderPosts()}
-      </ul>
-    </BasePage>
-      
+      <BasePage>
+        <h1>I am Portfolio Page</h1>
+        { loading && 
+        <p>Loading data...</p>
+        }
+        { posts &&
+          <ul>
+            {renderPosts(posts)}
+          </ul>
+        }
+        { error &&
+          <div className="alert alert-danger">{error.message}</div>
+        }
+      </BasePage>
     </BaseLayout>
   )
 }
 
-// Portfolios.getInitialProps = async () => {
-//   let posts = [];
-// try {
-//   const res = await axios.get("https://jsonplaceholder.typicode.com/posts")
-//   posts = res.data;
-// } catch(e) {
-//   console.error(e)
-// }
-//   return {posts: posts.slice(0,10)}
-// }
-
-export default Portfolios
+export default Portfolios;
